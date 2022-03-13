e684a54435a03164f79794fac0316182ed82072c
eloukas <eleftheriosloukas@gmail.com>
2022-03-13 20:58:51 +0200

    Update helper

--- libs/helper.py
+++ libs/helper.py
@@ -8,11 +8,15 @@ CORPUS_FILEPATH = os.path.join(DATA_FILEPATH, 'corpus_jobs.jsonl')
 
 LABEL_ENCODERS_FILEPATH = os.path.join(PROJECT_FILEPATH, 'label_encoders')
 VECTORIZERS_FILEPATH = os.path.join(PROJECT_FILEPATH, 'vectorizers')
+MODEL_FILEPATH = os.path.join(PROJECT_FILEPATH, 'model')
+
+DEV_GOLD_ANSWERS = os.path.join(DATA_FILEPATH, 'dev_gold_answers.jsonl')
+CORPUS_GOLD_ANSWERS = os.path.join(DATA_FILEPATH, ' corpus_pairs')
 
 # Column lists
 TEXT_COLS = ['title', 'description', 'requirement_summary', 'benefit_summary', 'user_keywords', 'soft_skills',
              'technical_skills']
-
+COLUMNS_WITH_ARRAYS = ['user_keywords', 'soft_skills', 'technical_skills']
 COLUMNS_TO_DELETE = ['created_at', 'industry', 'benefit_summary']
 COLUMNS_TO_ENUMERATE = ['employment_type', 'function', 'education', 'collar_color']
 COLUMNS_TO_COUNT_FREQUENCIES = ['title', 'description', 'requirement_summary', 'user_keywords', 'soft_skills',
